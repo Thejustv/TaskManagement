dcbc29c42c01ddc647c07411bc6519e1de600c6e
Thejustv <thejustv22@gmail.com>
2024-03-25 12:32:16 +0530

    minor changes

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import { FIREBASE_API } from "./constants";
+
+const firebaseConfig = {
+    apiKey: FIREBASE_API,
+    authDomain: "taskmanagement-e15f3.firebaseapp.com",
+    projectId: "taskmanagement-e15f3",
+    storageBucket: "taskmanagement-e15f3.appspot.com",
+    messagingSenderId: "835721905018",
+    appId: "1:835721905018:web:d4b2a935b71214147f0cc3",
+    measurementId: "G-JZ1P8TVME3"
+  }; 
+
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+
+  export const auth = getAuth(firebaseApp);
+  
+  export { db }
