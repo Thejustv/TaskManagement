4cff93c8d86d6ae882bd299fc8c3fdee2bcb97be
Thejustv <thejustv22@gmail.com>
2024-03-25 13:08:27 +0530

    small changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -93,7 +93,7 @@ function SignIn() {
           </Button>
           <Grid container>
             <Grid item>
-              <Link href="/signup" variant="body2">
+              <Link to={"/signup"} variant="body2">
                 {"Don't have an account? Sign Up"}
               </Link>
             </Grid>
